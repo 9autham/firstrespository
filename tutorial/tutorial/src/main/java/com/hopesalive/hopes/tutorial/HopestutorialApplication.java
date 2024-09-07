c3f92d942552e5adfebda4360b657bf4c1e12f67
Gautham <gautam9.2016@gmail.com>
2024-09-07 18:51:31 +0530

    Intial Commit

---
+++ tutorial/tutorial/src/main/java/com/hopesalive/hopes/tutorial/HopestutorialApplication.java
@@ -0,0 +1,13 @@
+package com.hopesalive.hopes.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopestutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopestutorialApplication.class, args);
+	}
+
+}
